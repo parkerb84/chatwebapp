8bcf271b26cec3a75b717f0fe6690df57dd9f48c
parkerb84 <fenratsu@gmail.com>
2018-07-23 15:55:26 -0400

    Added mLab MongoDB connection in Server.js

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+var dbUrl = 'mongodb://user:Tester1@ds245901.mlab.com:45901/chatwebapp'
 
 var messages = [
   {name: "Tim", message: "Hi"},
@@ -25,6 +28,10 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('A user connected')
+})
+
+mongoose.connect(dbUrl, {useNewUrlParser: true}, (err) => {
+  console.log('Mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
